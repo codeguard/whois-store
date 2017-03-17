04568d1c8156952224a7cc08a5d2f44c2527590e
bangms92 <bangms92@gmail.com>
2017-03-17 06:12:40 -0400

    Domain populating WHOIS data
    WHOIS data is populated using name as domain.
    
    e.g.
    domain = Domain.new(:name => “google.com”)
    domain.populate

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,5 +1,9 @@
-class Domain < ApplicationRecord
+require 'whois'
 
+class Domain < ApplicationRecord
   def populate
+  	name =  self.name
+  	record = Whois.whois(name)
+  	self.whois_response = record
   end
-end
+end	
