b9a32d221ff8efd5fdca2ab0f157249eb9078c80
bangms92 <bangms92@gmail.com>
2017-03-29 05:27:55 -0400

    Added some tests

--- test/models/domain_test.rb
+++ test/models/domain_test.rb
@@ -8,4 +8,25 @@ class DomainTest < ActiveSupport::TestCase
     domain = Domain.new(name: "foo.com", whois_response: "WHOIS DATA")
     assert_not_nil domain.populate 
   end
+
+  # incorrect domain name raises error
+  test "populate with invalid domain name sholud raise error" do
+  	domain = Domain.new(name: "foocom", whois_response: "WHOIS DATA")
+  	assert_raise do
+  		domain.populate
+  	end
+  end
+
+  # This test is intended to fail to indicate that future work is needed to treat these domains equally
+  test "These domains should be treated equally" do
+  	domain = Domain.new(name: "foo.com", whois_response: "WHOIS DATA")
+  	domain2 = Domain.new(name: "www.foo.com", whois_response: "WHOIS DATA")
+  	assert_equal(domain.name,domain2.name)
+  end
+
+  # check regex if valid domain name is given 
+  test "invalid domain name is entered" do
+  	domain = Domain.new(name: "foocom", whois_response: "WHOIS DATA")
+  	assert_no_match("/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/", domain.name, "Incorrect format for domain name")
+  end
 end
