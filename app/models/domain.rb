a7ba9cd4fb1a1fb6d0ea72d935243098f7e509ec
bangms92 <bangms92@gmail.com>
2017-04-02 02:11:28 -0400

    validation of domain name added

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,8 @@
 require 'whois'
 
 class Domain < ApplicationRecord
+	validates_format_of :name, :with => /\A(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$\Z/
+
   def populate
   	name =  self.name
   	record = Whois.whois(name)
